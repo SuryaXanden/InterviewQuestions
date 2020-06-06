3883275051d8b92837b202acc78d742b0b027c67
Surya U <suryaxanden@gmail.com>
2020-06-07 00:13:19 +0530

    Update Horse-racing Duals.js

--- Horse-racing Duals.js
+++ Horse-racing Duals.js
@@ -1,27 +1,14 @@
-const N = parseInt(readline());
+const N = +(readline());
 let indexes = []
 for (let i = 0; i < N; i++) {
     indexes.push(+readline())
 }
-// indexes = indexes.sort()
-let differences = {}
-for( let i = 0 ; i < indexes.length ; i ++ ){
-    for( let j = 0 ; j < indexes.length ; j ++ ){
-        if( i != j ){
-            let difference = Math.abs( indexes[i] - indexes[j] )
-            if( difference ){
-                const additional = [indexes[i] , indexes[j]].sort()
-                if( difference in differences ){
-                    differences[difference].concat( additional )
-                }
-                else{
-                    differences[ difference ] = additional
-                }
-            }
-        }
+indexes = indexes.map(i => +i).sort((a, b) => a - b)
+let min = Infinity
+for (let i = 0; i < indexes.length - 1; i++) {
+    let diff = indexes[i + 1] - indexes[i]
+    if (diff < min) {
+        min = diff
     }
 }
-const smallestDifference = Object.keys(differences).map(i=>+i)[0]
-const valuesWithSmallestDifference = differences[ smallestDifference ]
-// print( differences[ smallestDifference ].length )
-print( smallestDifference )
+console.log(min)
